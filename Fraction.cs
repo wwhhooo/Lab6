4506eb084865889304eaf5e6d9772d7a0d2b0da9
wwhhooo <146121665+wwhhooo@users.noreply.github.com>
2024-12-13 13:41:51 +0500

    Update and rename Fractions.cs to Fraction.cs

--- Fraction.cs
+++ Fraction.cs
@@ -1,60 +1,74 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Lab_6
 {
-    // Интерфейс для работы с дробями
-    public interface IFraction
-    {
-        double GetRealValue();
-        void SetNumerator(int numerator);
-        void SetDenominator(int denominator);
-    }
-
     // Класс для работы с дробями
     public class Fraction : IFraction, ICloneable
     {
+        // Свойство для числителя
         public int Numerator { get; private set; }
+
+        // Свойство для знаменателя
         public int Denominator { get; private set; }
 
-        private double? IsRealValue;
+        // Кэшированное вещественное значение дроби
+        private double? cachedRealValue;
 
+        // Конструктор класса Fraction
         public Fraction(int numerator, int denominator)
         {
+            // Проверка на ноль в знаменателе
             if (denominator == 0)
             {
                 throw new ArgumentException("Знаменатель не может быть равен нулю.");
             }
+
+            // Нормализация знаменателя и числителя
             if (denominator < 0)
             {
                 numerator = -numerator;
                 denominator = -denominator;
             }
+
+            // Установка числителя и знаменателя
             Numerator = numerator;
             Denominator = denominator;
+
+            // Сокращение дроби
             Simplify();
         }
 
+        // Переопределение метода ToString для вывода дроби в строковом формате
         public override string ToString()
         {
+            // Если знаменатель равен 1, возвращаем только числитель
             if (Denominator == 1)
             {
                 return Numerator.ToString();
             }
+
+            // Возвращаем дробь в формате "числитель/знаменатель"
             return $"{Numerator}/{Denominator}";
         }
 
+        // Метод для сокращения дроби
         private void Simplify()
         {
+            // Нахождение наибольшего общего делителя (НОД)
             int gcd = GCD(Numerator, Denominator);
+
+            // Сокращение числителя и знаменателя
             Numerator /= gcd;
             Denominator /= gcd;
-            IsRealValue = null; // Сделать недействительным кэшированное реальное значение
+
+            // Сброс кэшированного вещественного значения
+            cachedRealValue = null;
         }
 
+        // Метод для нахождения наибольшего общего делителя (НОД)
         private int GCD(int a, int b)
         {
+            // Алгоритм Евклида для нахождения НОД
             while (b != 0)
             {
                 int temp = b;
@@ -64,6 +78,7 @@ namespace Lab_6
             return Math.Abs(a);
         }
 
+        // Перегрузка оператора сложения для дробей
         public static Fraction operator +(Fraction f1, Fraction f2)
         {
             int newNumerator = f1.Numerator * f2.Denominator + f2.Numerator * f1.Denominator;
@@ -71,6 +86,7 @@ namespace Lab_6
             return new Fraction(newNumerator, newDenominator);
         }
 
+        // Перегрузка оператора вычитания для дробей
         public static Fraction operator -(Fraction f1, Fraction f2)
         {
             int newNumerator = f1.Numerator * f2.Denominator - f2.Numerator * f1.Denominator;
@@ -78,6 +94,7 @@ namespace Lab_6
             return new Fraction(newNumerator, newDenominator);
         }
 
+        // Перегрузка оператора умножения для дробей
         public static Fraction operator *(Fraction f1, Fraction f2)
         {
             int newNumerator = f1.Numerator * f2.Numerator;
@@ -85,34 +102,42 @@ namespace Lab_6
             return new Fraction(newNumerator, newDenominator);
         }
 
+        // Перегрузка оператора деления для дробей
         public static Fraction operator /(Fraction f1, Fraction f2)
         {
+            // Проверка на деление на ноль
             if (f2.Numerator == 0)
             {
                 throw new DivideByZeroException("Деление на ноль.");
             }
+
             int newNumerator = f1.Numerator * f2.Denominator;
             int newDenominator = f1.Denominator * f2.Numerator;
             return new Fraction(newNumerator, newDenominator);
         }
 
+        // Перегрузка оператора сложения дроби с целым числом
         public static Fraction operator +(Fraction f, int n)
         {
             return f + new Fraction(n, 1);
         }
 
+        // Перегрузка оператора вычитания дроби с целым числом
         public static Fraction operator -(Fraction f, int n)
         {
             return f - new Fraction(n, 1);
         }
 
+        // Перегрузка оператора умножения дроби на целое число
         public static Fraction operator *(Fraction f, int n)
         {
             return f * new Fraction(n, 1);
         }
 
+        // Перегрузка оператора деления дроби на целое число
         public static Fraction operator /(Fraction f, int n)
         {
+            // Проверка на деление на ноль
             if (n == 0)
             {
                 throw new DivideByZeroException("Деление на ноль.");
@@ -120,6 +145,7 @@ namespace Lab_6
             return f / new Fraction(n, 1);
         }
 
+        // Переопределение метода Equals для сравнения дробей
         public override bool Equals(object obj)
         {
             if (obj == null || GetType() != obj.GetType())
@@ -131,34 +157,40 @@ namespace Lab_6
             return Numerator == other.Numerator && Denominator == other.Denominator;
         }
 
+        // Переопределение метода GetHashCode для получения хэш-кода дроби
         public override int GetHashCode()
         {
             return HashCode.Combine(Numerator, Denominator);
         }
 
-
+        // Реализация метода Clone из интерфейса ICloneable
         public object Clone()
         {
             return new Fraction(Numerator, Denominator);
         }
 
+        // Реализация метода GetRealValue из интерфейса IFraction
         public double GetRealValue()
         {
-            if (IsRealValue == null)
+            // Если кэшированное значение отсутствует, вычисляем его
+            if (cachedRealValue == null)
             {
-                IsRealValue = (double)Numerator / Denominator;
+                cachedRealValue = (double)Numerator / Denominator;
             }
-            return IsRealValue.Value;
+            return cachedRealValue.Value;
         }
 
+        // Реализация метода SetNumerator из интерфейса IFraction
         public void SetNumerator(int numerator)
         {
             Numerator = numerator;
             Simplify();
         }
 
+        // Реализация метода SetDenominator из интерфейса IFraction
         public void SetDenominator(int denominator)
         {
+            // Проверка на ноль в знаменателе
             if (denominator == 0)
             {
                 throw new ArgumentException("Знаменатель не может быть равен нулю.");
